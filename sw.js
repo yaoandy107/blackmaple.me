55831441f1cdc66d1c8aa0d8ed0270b5c161cc17
yaoandy107 <yaoandy107@gmail.com>
2018-10-24 15:47:16 +0800

    Update cache policy

--- sw.js
+++ sw.js
@@ -23,17 +23,9 @@ workbox.routing.registerRoute(
 workbox.routing.registerRoute(
   // Cache image files
   /.*\.(?:png|jpg|jpeg|svg|gif)/,
-  // Use the cache if it's available
+  // Use cache but update in the background ASAP
   workbox.strategies.staleWhileRevalidate({
     // Use a custom cache name
-    cacheName: 'image-cache',
-    plugins: [
-      new workbox.expiration.Plugin({
-        // Cache only 20 images
-        maxEntries: 20,
-        // Cache for a maximum of a week
-        maxAgeSeconds: 7 * 24 * 60 * 60
-      })
-    ]
+    cacheName: 'image-cache'
   })
 );
